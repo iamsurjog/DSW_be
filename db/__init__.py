a6b0e1079dca61a4a963959f72924749d6371bac
iamsurjog <iamsurjog@gmail.com>
2025-10-20 23:31:30 +0530

    feat: sample db connection shit

--- db/__init__.py
+++ db/__init__.py
@@ -3,7 +3,12 @@ import sqlite3
 conn = sqlite3.connect('db.db')
 cursor = conn.cursor()
 
-print(cursor)
+def query(query: str):
+    cursor.execute(query)
+    if "select" in query.casefold():
+        return cursor.fetchall()
 
-cursor.close()
+conn.commit()
+
+
 
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 import db
+
+db.query("Create table emp(name varchar(10), age number(2))")
+db.query("insert into emp values ('this', 2)")
+print(db.query("Select * from emp"))
+db.query("drop table emp")
