0eeffb7dbe3ec428c3f5fcf0981b2eaead90ef13
iamsurjog <iamsurjog@gmail.com>
2025-10-21 17:06:09 +0530

    feat: cleaner?

--- db/__init__.py
+++ db/__init__.py
@@ -1,14 +1,9 @@
-import sqlite3
-
-conn = sqlite3.connect('db.db')
-cursor = conn.cursor()
-
-def query(query: str):
+def query(cursor, query: str):
     cursor.execute(query)
     if "select" in query.casefold():
         return cursor.fetchall()
 
-conn.commit()
-
+def commit(cursor):
+    cursor.commit()
 
 
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import db
+import sqlite3
 
-db.query("Create table emp(name varchar(10), age number(2))")
-db.query("insert into emp values ('this', 2)")
-print(db.query("Select * from emp"))
-db.query("drop table emp")
+conn = sqlite3.connect('db.db')
+cursor = conn.cursor()
+
+
+db.query(cursor, "Create table emp(name varchar(10), age number(2))")
+db.query(cursor, "insert into emp values ('this', 2)")
+print(db.query(cursor, "Select * from emp"))
+db.query(cursor, "drop table emp")
+conn.commit()
